25abe061bb4b27498be02a4d817d842fb2c8f7dc
filecc <fil.gia@live.it>
2023-11-28 14:09:31 +0100

    Update package-lock.json and package.json
    The commit message suggests that the changes made are related to
    updating the package-lock.json and package.json files.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const port = process.env.PORT
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 
 
@@ -13,6 +14,13 @@ const errorMiddleware = require('./middleware/errors')
 const userRouter = require('./routers/user')
 
 app.get('/favicon.ico', (req, res) => res.status(204))
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  optionSuccessStatus: 200,
+  methods: 'GET,POST,PUT'
+}
+app.use(cors());
 app.use(express.json())
 app.use(express.static('public'))
 
